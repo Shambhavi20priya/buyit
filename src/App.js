48fde442307c866788c23f7d83df868502205a34
Shambhavi Priya <shambhavipriya2000@gmail.com>
2023-04-14 02:43:21 +0530

    About, contact, wishlist page design completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import About from "./pages/About"
 import OurStore from './pages/OurStore';
+import Blog from './pages/Blog';
+import CompareProduct from './pages/CompareProduct';
+import Wishlist from './pages/Wishlist';
+import Login from './pages/Login';
 function App() {
   return (
   <>
@@ -16,6 +20,10 @@ function App() {
           <Route path='contact' element={<Contact />} />
           <Route path ='about' element={<About />}></Route>
           <Route path ='store' element={<OurStore />}></Route>
+          <Route path ='blogs' element={<Blog />}></Route>
+          <Route path ='compare-product' element={<CompareProduct />}></Route>
+          <Route path ='wishlist' element={<Wishlist />}></Route>
+          <Route path ='login' element={<Login />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Meta from "../components/Meta";
+const Login = () => {
+  return (
+    <>
+     <Meta title={"Login"} />
+
+
+      </>
+
+  )
+}
+
+export default Login
